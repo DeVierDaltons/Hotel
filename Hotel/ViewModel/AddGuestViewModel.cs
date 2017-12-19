04bc31603bb00b9cced0553952d2990b7a0d8e90
djsleurink <djsleurink@hotmail.com>
2017-12-19 10:34:34 +0100

    Added functionality to add a guest to the hotelguestslist using commands and notifypropertychanged

--- Hotel/ViewModel/AddGuestViewModel.cs
+++ Hotel/ViewModel/AddGuestViewModel.cs
@@ -1,49 +1,66 @@
-﻿using Hotel.Model;
+﻿using Hotel.Command;
+using Hotel.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Hotel.ViewModel
 {
-    class AddGuestViewModel
+    class AddGuestViewModel : INotifyPropertyChanged
     {
-        public Guest guest { get; set; } = new Guest();
+
 
         #region Properties
+        public ICommand AddGuestCommand { get; set; }
+        public List<Guest> HotelGuestsList { get; set; } = new List<Guest>();
+        public Guest guest { get; set; } = new Guest();
+        public event PropertyChangedEventHandler PropertyChanged;
         public string FirstName
         {
             get { return guest.FirstName; }
-            set { guest.FirstName = value; }
+            set { guest.FirstName = value; OnPropertyChanged(nameof(FirstName)); }
         }
 
         public string LastName
         {
             get { return guest.LastName; }
-            set { guest.LastName = value; }
+            set { guest.LastName = value; OnPropertyChanged(nameof(LastName)); }
         }
 
         public string PhoneNumber
         {
             get { return guest.PhoneNumber; }
-            set { guest.PhoneNumber = value; }
+            set { guest.PhoneNumber = value; OnPropertyChanged(nameof(PhoneNumber)); }
         }
 
         public string EmailAdress
         {
             get { return guest.EmailAdress; }
-            set { guest.EmailAdress = value; }
+            set { guest.EmailAdress = value; OnPropertyChanged(nameof(EmailAdress)); }
         }
         public string ICEPhoneNumber
         {
             get { return guest.ICEPhoneNumber; }
-            set { guest.ICEPhoneNumber = value; }
+            set { guest.ICEPhoneNumber = value; OnPropertyChanged(nameof(ICEPhoneNumber)); }
         }
 
         #endregion Properties
         public AddGuestViewModel()
         {
+            AddGuestCommand = new AddGuestCommand(this);
+        }
+        internal bool ValidateInput()
+        {
+            return true ;
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
     }
