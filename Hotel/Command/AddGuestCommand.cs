f443a6d33d5568e33df90f29e9336171e5621f10
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-19 16:22:21 +0100

    removed unused usings, useless comments, made classes public

--- Hotel/Command/AddGuestCommand.cs
+++ Hotel/Command/AddGuestCommand.cs
@@ -1,42 +1,29 @@
-﻿using Hotel.Model;
-using Hotel.ViewModel;
+﻿using Hotel.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Hotel.Command
 {
-     class AddGuestCommand : ICommand
+    public class AddGuestCommand : ICommand
     {
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
-        AddGuestViewModel viewModel;
+
+        private AddGuestViewModel viewModel;
 
         public AddGuestCommand(AddGuestViewModel vm)
         {
             viewModel = vm;
         }
 
-        /// <summary>
-        /// Validates the fields in the add guest window
-        /// </summary>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return viewModel.ValidateInput();
         }
 
-        /// <summary>
-        /// Adds a new guest to the list of guests in the hotel.
-        /// </summary>
-        /// <param name="parameter"></param>
         public void Execute(object parameter)
         {
             viewModel.AddGuest();
--- Hotel/Command/AddRoomCommand.cs
+++ Hotel/Command/AddRoomCommand.cs
@@ -1,15 +1,10 @@
-﻿using Hotel.Model;
-using Hotel.ViewModel;
+﻿using Hotel.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Hotel.Command
 {
-     class AddRoomCommand : ICommand
+    public class AddRoomCommand : ICommand
     {
         public event EventHandler CanExecuteChanged
         {
@@ -17,27 +12,18 @@ namespace Hotel.Command
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        AddRoomViewModel viewModel;
+        private AddRoomViewModel viewModel;
 
         public AddRoomCommand(AddRoomViewModel vm)
         {
             viewModel = vm;
         }
 
-        /// <summary>
-        /// Validates the fields in the add guest window
-        /// </summary>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return viewModel.ValidateInput();
         }
 
-        /// <summary>
-        /// Adds a new guest to the list of guests in the hotel.
-        /// </summary>
-        /// <param name="parameter"></param>
         public void Execute(object parameter)
         {
             viewModel.AddRoom();
