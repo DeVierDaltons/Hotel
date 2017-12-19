da956c486e0d4cb61a9251fd3c9be323dd4f8b84
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-19 06:26:34 +0100

    Added AddRoomView.xaml with AddRoomViewModel

---
+++ Hotel/ViewModel/AddRoomViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Hotel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.ViewModel
+{
+    public class AddRoomViewModel
+    {
+        public Room room { get; set; } = new Room();
+
+        private int _roomNumber;
+
+        public int RoomNumber
+        {
+            get { return _roomNumber; }
+            set { _roomNumber = value; }
+        }
+        
+    }
+}
