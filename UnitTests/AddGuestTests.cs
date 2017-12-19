f7de0fe1e401ba31488f1aff5154f027569265a9
djsleurink <djsleurink@hotmail.com>
2017-12-19 16:37:41 +0100

    Added unit test project

---
+++ UnitTests/AddGuestTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddGuestTest()
+        {
+        }
+
+        [TestMethod]
+        public void AddInvalidGuestTest()
+        {
+
+        }
+    }
+}
