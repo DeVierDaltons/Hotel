7fba5f186ad022f0b043a46b4cff7a6133464a1b
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-22 13:12:46 +0100

    fixed representation of bookings in Bookings window

--- Hotel/Model/Booking.cs
+++ Hotel/Model/Booking.cs
@@ -11,6 +11,26 @@ namespace Hotel.Model
         public DateTime StartDay;
         public DateTime EndDay;
 
+        public string StartDayString
+        {
+            get { return StartDay.ToShortDateString(); }
+        }
+
+        public string EndDayString
+        {
+            get { return EndDay.ToShortDateString(); }
+        }
+
+        public string GuestName
+        {
+            get { return Guest.FirstName ?? "null"; }
+        }
+
+        public string RoomNumber
+        {
+            get { return Room.RoomNumber ?? "null"; }
+        }
+
         public void SetDates(SelectedDatesCollection selectedDates)
         {
             StartDay = selectedDates.FirstOrDefault();
