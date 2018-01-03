290bff1dc725913adb20b8b12bd94d946dc9f430
Tama McGlinn <t.mcglinn@gmail.com>
2018-01-03 16:21:25 +0100

    removed commands for opening windows

--- Hotel/ViewModel/MainWindowViewModel.cs
+++ Hotel/ViewModel/MainWindowViewModel.cs
@@ -14,12 +14,6 @@ namespace Hotel.ViewModel
     public class MainWindowViewModel
     {
         public HotelManager HotelManager { get; set; }
-        public ICommand ShowAddGuestWindowCommand { get; }
-        public ICommand ShowGuestsWindowCommand { get; }
-        public ICommand ShowAddRoomWindowCommand { get; }
-        public ICommand ShowAddModifyRoomWindow { get; }
-        public ICommand ShowAddBookingWindowCommand { get; }
-        public ICommand ShowBookingsWindowCommand { get; }
 
         public MainWindowViewModel()
         {
@@ -28,13 +22,6 @@ namespace Hotel.ViewModel
             HotelManager = new HotelManager(new NHibernateRepository<Guest>(), 
                                             new NHibernateRepository<Room>(), 
                                             new NHibernateRepository<Booking>());
-            ShowAddGuestWindowCommand = new ShowGuestDetailWindowCommand(new AddGuestCommand(HotelManager.Guests));
-            ShowGuestsWindowCommand = new ShowGuestsWindowCommand(HotelManager);
-            ShowAddRoomWindowCommand = new OpenAddRoomWindowCommand(HotelManager);
-            ShowAddBookingWindowCommand = new OpenAddBookingWindowCommand(HotelManager);
-            ShowAddModifyRoomWindow = new OpenModifyRoomWindowCommand(HotelManager);
-            ShowAddModifyRoomWindow = new OpenModifyRoomWindowCommand(HotelManager);
-            ShowBookingsWindowCommand = new OpenModifyBookingWindowCommand(HotelManager);
         }
     }
 }
