16165bdc596c8f19d4eda3b219647ab92fae73cf
Tama McGlinn <t.mcglinn@gmail.com>
2018-01-08 14:44:40 +0100

    removed useless comments in IRepository

--- Hotel/Repository/IRepository.cs
+++ Hotel/Repository/IRepository.cs
@@ -5,35 +5,10 @@ namespace Hotel.Repository
 {
     public interface IRepository<T>
     {
-        /// <summary>
-        /// Get item entity by id
-        /// </summary>
-        /// <param name="id">id</param>
-        /// <returns>item</returns>
         T Get(Guid guid);
-
-        /// <summary>
-        /// Save item entity
-        /// </summary>
-        /// <param name="item">item</param>
         void Save(T item);
-
-        /// <summary>
-        /// Update item entity
-        /// </summary>
-        /// <param name="item">item</param>
         void Update(T item);
-
-        /// <summary>
-        /// Delete item entity
-        /// </summary>
-        /// <param name="item">item</param>
         void Delete(T item);
-
-        /// <summary>
-        /// Retrieve all guests
-        /// </summary>
-        /// <returns>All the guests</returns>
         List<T> GetAll();
     }
 }
