75d5da6f851785b10f8a9a3881287ae686776d5e
Tama McGlinn <t.mcglinn@gmail.com>
2018-01-09 10:11:32 +0100

    extremely hacky way of displaying coloured blocks

--- Hotel/ViewModel/RoomAvailability.cs
+++ Hotel/ViewModel/RoomAvailability.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Hotel.Model;
+using System.Windows.Media;
 
 namespace Hotel.ViewModel
 {
@@ -21,6 +22,9 @@ namespace Hotel.ViewModel
 
         public bool Day1 { get; private set; }
         public bool Day2 { get; private set; }
+
+        public string Color1 { get { return Day1 ? "Green" : "Red";  } }
+        public string Color2 { get { return Day2 ? "Green" : "Red";  } }
 
         private Room room;
 
