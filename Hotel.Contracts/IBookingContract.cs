9c514b8ae26c75c097c97ead5f88950a836a8b4c
djsleurink <djsleurink@hotmail.com>
2018-01-16 11:10:04 +0100

    Setting up contracts

---
+++ Hotel.Contracts/IBookingContract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Hotel.Contracts
+{
+    [DataContract]
+    public class BookingData
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+    }
+}
