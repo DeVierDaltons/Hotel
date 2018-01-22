dd2107411790dfa0596c520a57983011e8b0faf1
Tama McGlinn <t.mcglinn@gmail.com>
2018-01-22 14:05:04 +0100

    added missing projects; host, data, services

---
+++ Hotel.Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using Hotel.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hotelServiceHost = new ServiceHost(typeof(HotelService));
+            hotelServiceHost.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to quit.");
+            Console.ReadLine();
+
+            hotelServiceHost.Close();
+        }
+    }
+}
---
+++ Hotel.Services/HotelService.cs
@@ -0,0 +1,17 @@
+﻿using Hotel.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Services
+{
+    public class HotelService : IHotelService
+    {
+        public void DummyOperation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
