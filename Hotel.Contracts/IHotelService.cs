c1cfdbe985acbf295058776e8d141bbaffdb8d68
djsleurink <djsleurink@hotmail.com>
2018-01-22 15:38:19 +0100

    Contract now has crud operations

--- Hotel.Contracts/IHotelService.cs
+++ Hotel.Contracts/IHotelService.cs
@@ -1,6 +1,7 @@
 ﻿using Hotel.Data;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -11,7 +12,38 @@ namespace Hotel.Contracts
     [ServiceContract]
     public interface IHotelService
     {
+        //Guest operations
         [OperationContract]
         void AddGuest(Guest guest);
+        [OperationContract]
+        void RemoveGuest(Guest guest);
+        [OperationContract]
+        void EditGuest(Guest guest);
+        [OperationContract]
+        ObservableCollection<Guest> GetAllGuests();
+        [OperationContract]
+        ObservableCollection<Guest> FilterGuests(string filterString);
+
+        //Booking operations
+        [OperationContract]
+        void AddBooking(Booking booking);
+        [OperationContract]
+        void RemoveBooking(Booking booking);
+        [OperationContract]
+        void EditBooking(Booking booking);
+        [OperationContract]
+        ObservableCollection<Booking> GetAllBookings();
+        [OperationContract]
+        ObservableCollection<Guest> FilterBookings(BookingStatus status, string filterString);
+
+        //Room operations
+        [OperationContract]
+        void AddRoom(Room room);
+        [OperationContract]
+        void RemoveRoom(Room room);
+        [OperationContract]
+        void EditRoom(Room room);
+        [OperationContract]
+        ObservableCollection<Room> GetAllRooms();
     }
 }
