ce8f0edf1a7dd3efdd5f525a094213f0e64f00b8
djsleurink <djsleurink@hotmail.com>
2018-01-22 16:30:50 +0100

    Adding changetracker

---
+++ Hotel.Data/ChangeTracker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Data
+{
+    public class ChangeTracker : IIdentifiable
+    {
+        public virtual Guid Id { get; set; }
+        public virtual Guid ChangedObject { get; set; }
+        public virtual DateTime Timestamp { get; set; }
+        public virtual Type ChangedObjectType { get; set; }
+    }
+}
---
+++ Hotel.Data/DataAccessObjects/ChangeTrackerMap.cs
@@ -0,0 +1,13 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Data.DataAccessObjects
+{
+    public class ChangeTrackerMap : ClassMapping<ChangeTracker>
+    {
+    }
+}
