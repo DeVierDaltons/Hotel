37fc2b6bcac62e208f27e230ce06c5bf98ab3471
djsleurink <djsleurink@hotmail.com>
2018-01-22 15:30:32 +0100

    Added proxy

---
+++ Hotel.Proxy/HotelServiceProxy.cs
@@ -0,0 +1,19 @@
+﻿using Hotel.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.Data;
+
+namespace Hotel.Proxy
+{
+    public class HotelServiceProxy : ClientBase<IHotelService>, IHotelService
+    {
+        public void AddGuest(Guest guest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
