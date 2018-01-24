81c0d14f9a24dd517408be4d3e16a4a33fe54108
Tama McGlinn <t.mcglinn@gmail.com>
2018-01-24 15:45:29 +0100

    removed commented code

--- Hotel/App.xaml.cs
+++ Hotel/App.xaml.cs
@@ -12,32 +12,9 @@ namespace Hotel
         {
             IUnityContainer container = new UnityContainer();
             MainWindow window = container.Resolve<MainWindow>();
-            //InitializeRoomBookings(container);
-           
             window.Initialize();
             window.Show();
         }
-        /*
-        private void InitializeRoomBookings(IUnityContainer container)
-        {
-            var bookings = container.Resolve<RepositoryBackedObservableCollection<Booking>>();
-            bookings.ForEach((booking) =>
-            {
-                booking.Rooms.ForEach(room => room.Bookings.Add(booking));
-            });
-            bookings.CollectionChanged += (object sender, NotifyCollectionChangedEventArgs e) =>
-            {
-                if (e.OldItems != null)
-                {
-                    Booking removedBooking = (Booking)e.OldItems[0];
-                    removedBooking.Rooms.ForEach(room => room.Bookings.Remove(removedBooking));
-                }
-                if (e.NewItems != null)
-                {
-                    Booking addedBooking = (Booking)e.NewItems[0];
-                    addedBooking.Rooms.ForEach(room => room.Bookings.Add(addedBooking));
-                }
-            };*/
-        }
     }
+}
 
